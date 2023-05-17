e28ccb60d1fe107a76df38e01b45934af90a55a4
ri4n4djust <rian.adjust@gmail.com>
2023-05-17 18:23:43 +0800

    Update penjualanController.php

--- app/Http/Controllers/penjualanController.php
+++ app/Http/Controllers/penjualanController.php
@@ -307,12 +307,13 @@ class penjualanController extends Controller
 
         $header = DB::table('tblpenjualan')->where('noPenjualan', $noPenjualan)->first();
         $data = PenjualanDetail::where('r_noPenjualan', $noPenjualan)->get();
-        $array = json_decode(json_encode($data), true);
-        $combined = array_merge($header, $array);
-        return response()->json([
-            'success' => true,
-            'message' => 'Detail Penjualan!',
-            'data' => $combined
-        ], 200);
+        print_r($header)
+        // $array = json_decode(json_encode($data), true);
+        // $combined = array_merge($header, $array);
+        // return response()->json([
+        //     'success' => true,
+        //     'message' => 'Detail Penjualan!',
+        //     'data' => $combined
+        // ], 200);
     }
 }
